34780ae20457776a1eaa852bdca983a36e4aa106
Vinh Ngu <v.ngu@mmkh.de>
2018-03-01 22:39:11 +0100

    Print probability.

--- who_am_i.py
+++ who_am_i.py
@@ -58,13 +58,13 @@ def build_training_set():
 
         if len(faces) is 1:
 
+            print("I think you are ...")
             face_image = camera_image[faces[0][1]:faces[0][1]+faces[0][3], faces[0][0]:faces[0][0]+faces[0][2]]
             response = send_request(face_image)
-            print("I think you are ...")
-            print("... ",response.json()["label"])
+            print(response.json())
+            print("... ",response.json()["label"], "probably ... ", response.json()["probability"])
             # Write the image.
             filename = name + str(iterator) + '.jpg'
-            print('<face_extractor.py> Save image: ' + filename)
             cv.imwrite(new_path_str + '/' + filename, face_image)
 
             # Iterate iterator.
