b360098268455b1c50afb893101351ccf1ad0e4d
Christian Reichel <chrstn.rchl@gmail.com>
2018-03-04 00:00:28 +0100

    Fix paths. Add Canny Edge Detector as image enhancing method. Add classifier path return method.

--- face_extractor.py
+++ face_extractor.py
@@ -18,21 +18,32 @@
 # [ ] Replace image face_extractor with keypoint face_extractor.
 #######################################################
 
-# IMPORTS
+# IMPORTS.
 import os               # path finding
 import cv2 as cv        # webcam image
 import time             # sleep time
 
+# Settings.
+path_to_trainingdata = os.getcwd() + '/data/training'
+canny_lower_threshold = 40
+canny_upper_threshold = 200
+
+
+# Path to classifier.
+classifier_path = os.path.dirname(__file__) + 'data/cascades/haarcascade_frontalface_default.xml'
+
+
 def build_training_set():
 
     # Pretrained haar cascade classifiers.
-    face_cascade = cv.CascadeClassifier(os.path.dirname(__file__) + 'data/cascades/haarcascade_frontalface_default.xml')
+    face_cascade = cv.CascadeClassifier(classifier_path)
 
-    # Ask for users name
+    # Ask for users name.
     name = input("<face_extractor.py> How is your name?\n")
 
-    # Make new dir if user not exists
-    new_path_str = os.getcwd() + '/data/training/' + name + '_' + str(hash(int))
+    new_path_str = path_to_trainingdata + '/' + name + '_' + str(hash(int))
+
+    # Make new dir if user not exists.
     print('<face_extractor.py> Hello ' + name + '. The algorithm will now learn to recognise your face. Please rotate it slowly to make the recognition more stable. The recording start now.')
     time.sleep(0.100)
     print('-------------------------')
@@ -55,7 +66,11 @@ def build_training_set():
 
         if len(faces) is 1:
 
+            # Get the camera image.
             face_image = camera_image[faces[0][1]:faces[0][1]+faces[0][3], faces[0][0]:faces[0][0]+faces[0][2]]
+
+            # Shows the image.
+            cv.imshow('Face extractor', face_image)
 
             # Write the image.
             filename = name + str(iterator) + '.jpg'
@@ -72,7 +87,19 @@ def build_training_set():
 
     # End procedures.
     print('<face_extractor.py> Ending process. ' + str(iterator) + ' images are created and saved into ' + new_path_str + '.')
+    cv.destroyAllWindows()
     camera.release()
+
+# Connverts an image into detected edges and returns it.
+def convert_image(image_array):
+    edges = cv.Canny(image_array,canny_lower_threshold,canny_upper_threshold)
+
+    return edges
+
+# Returns the classifier path.
+def get_classifier_path():
+    return classifier_path
+
 
 # Method call. Uncomment for debugging.
 # build_training_set()
